e5fbfdd846bc2a992e2022e82e7f9c039e14bbc8
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-08-24 21:57:25 +1000

    route fix

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+// Couldn't find homeRoutes
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
