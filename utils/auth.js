342ffdd578a32e4fb04152965e4915f69c7c343f
thorsten <90459268+ThorstenBeowulf@users.noreply.github.com>
2023-08-28 20:49:38 +1000

    removed redundant auth call

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,5 @@
 const withAuth = (req, res, next) => {
-  if (!req.session.logged_in && req.path !== '/') {
+  if (!req.session.logged_in) {
     if (req.path === '/patient-profile') {
       res.redirect('/patient-login');
     } else if (req.path === '/doctor-profile') {
