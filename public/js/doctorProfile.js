93506fbe582a7c7cf25f78f8669c41f20c8d0e20
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-09-02 17:55:01 +1000

    button for patient info

--- public/js/doctorProfile.js
+++ public/js/doctorProfile.js
@@ -1,8 +1,8 @@
 const moreInfo = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-    document.location.replace(`/${id}`);
-  }
+  //   if (event.target.hasAttribute('data-id')) {
+  const id = event.target.getAttribute('data-id');
+  document.location.replace(`/${id}`);
+  //   }
 };
 
-document.querySelector('.approve-button').addEventListener('click', moreInfo);
+document.querySelector('.med-cert-list').addEventListener('click', moreInfo);
